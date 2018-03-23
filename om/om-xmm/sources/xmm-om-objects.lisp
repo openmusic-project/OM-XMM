806623f8edd650cc67c895bb33a7be8572146f71
Paul Best <paulobest25@gmail.com>
2018-03-23 16:41:16 +0100

    new save to JSON func, 9 as global variable DESC_NUM

--- om/om-xmm/sources/xmm-om-objects.lisp
+++ om/om-xmm/sources/xmm-om-objects.lisp
@@ -4,31 +4,36 @@
 
 (in-package :om)
 
-(defclass xmmiae (om-cleanup-mixin data-stream)
-((iae-obj :accessor iae-obj :initform iae)
- (markers-track :accessor markers)))
+(defclass xmmiae (iae)
+  ((markers-track :initarg :markers :accessor markers))
+)
 
 
 
 (in-package :xmm)
 
-;(defclass xmmobj ()
-;((model-ptr :accessor model-ptr :initform nil)
-;))
+(defclass xmmobj ()
+((model-ptr :accessor model-ptr :initform nil)
+))
 
 (defun init-model()
   (let ((model-ptr (initXMM)))
 ))
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;run-model(list) :
 ;;list is a matrix of descriptors for 1 sample 
-;;descriptor matrix is a list of 9 descriptor vectors
+;;descriptor matrix is a list of descriptor vectors
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun run-model(list)
-  
-
-  (code-char (runXMM descr (length (car list) model-ptr)))
+(defmethod run-model ((self xmmobj) list)
+  (let ((descr (fli:allocate-foreign-object :type :pointer :nelems (length list)))
+        (size (length (car list))))
+    ;;Loop for each descriptor
+    (loop for i from 0 to (1- (length list))  
+          do (setf (fli:dereference descr :type :pointer :index i) 
+                   (fli:allocate-foreign-object :type :float :nelems size :initial-contents (nth i list))))
+  (code-char (runXMM descr (length (car list)) (model-ptr self))))
 )
 
 
@@ -39,16 +44,14 @@
 ;;learn(list) :
 ;;list is a list of samples. 
 ;;each sample is a list of size 2 : the descriptor matrix, and the label
-;;descriptor matrix is a list of 9 descriptor vectors
+;;descriptor matrix is a list of descriptor vectors
 ;;each descriptor vector is a list of values 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun learn(list)
+(defmethod learn ((self xmmobj) list)
   (let ((laabels (fli:allocate-foreign-object :type :char :nelems (length list)))
         (descrs (fli:allocate-foreign-object :type :pointer :nelems (length list)))
-        (sizes (fli:allocate-foreign-object :type :int :nelems (length list)))
-        ;; temporaire        
-        (model-ptr (initXMM)))
+        (sizes (fli:allocate-foreign-object :type :int :nelems (length list))))       
 
     ;;Loop for each sample
     (loop for j from 0 to (1- (length list))
@@ -62,7 +65,7 @@
                               (fli:allocate-foreign-object :type :float :nelems size :initial-contents (nth i (car (nth j list))))))))
     
     
-    (code-char (trainXMM descrs (length list) sizes laabels model-ptr))
+    (code-char (trainXMM descrs (length list) sizes laabels (model-ptr self)))
 ;;FREE MEMORY
 ))
 
--- xmm-om/sources/xmm-lib.cpp
+++ xmm-om/sources/xmm-lib.cpp
@@ -13,7 +13,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-
+static const int DESC_NUM=9;
 
 
 
@@ -40,7 +40,7 @@ int trainXMM(void* descptr, int sample_num, void* sample_sizes, void* labls, voi
     xmm::TrainingSet *mdataset = new xmm::TrainingSet(xmm::MemoryMode::OwnMemory, xmm::Multimodality::Unimodal);
     
     const std::vector<std::string> descr_names {"Total Energy","Fundamental Frequency","Spectral Centroid","Loudness","Sharpness","Spread","Harmonic Energy","Inharmonicity", "Noisiness"};
-    mdataset->column_names.resize(9);
+    mdataset->column_names.resize(DESC_NUM);
     mdataset->column_names.set(descr_names);
     mdataset->dimension=9;
     
@@ -52,13 +52,13 @@ int trainXMM(void* descptr, int sample_num, void* sample_sizes, void* labls, voi
             
             //Build Phrase
             mdataset->addPhrase(j, toString(labels[j]));
-            mdataset->getPhrase(j)->column_names.resize(9);
+            mdataset->getPhrase(j)->column_names.resize(DESC_NUM);
             mdataset->getPhrase(j)->column_names =descr_names;
-            mdataset->getPhrase(j)->dimension =9;
+            mdataset->getPhrase(j)->dimension =DESC_NUM;
             for(int it =0; it < sizes[j]; it++){
-                std::vector<float> observation = *new std::vector<float>(9);
+                std::vector<float> observation = *new std::vector<float>(DESC_NUM);
                 //Add each sound descriptor to the phrase
-                for(int i =0; i<9; i++){
+                for(int i =0; i<DESC_NUM; i++){
                     observation[i] = descr[j][i][it];
                 }
                 mdataset->getPhrase(j)->record(observation);
@@ -81,25 +81,22 @@ int trainXMM(void* descptr, int sample_num, void* sample_sizes, void* labls, voi
         std::cout<<"\n";
         
         std::cout<<"Num of obs : "<<mdataset->size()<<std::endl;
-        //////////////////////////////
-        //////////// TEST ////////////
-        //////////////////////////////
         
+        ////////////////////
+        /////// TEST ///////
+        ////////////////////
         float accuracy = 0;
+        char predicted = ' ';
         for(int j=0; j<sample_num;j++){
-        
-            
             mhhmm->reset();
             for(int k=0; k<sizes[j];k++){
-                std::vector<float> observation = *new std::vector<float>(9);
-        
-                for(int i =0; i<9; i++){
+                std::vector<float> observation = *new std::vector<float>(DESC_NUM);
+                for(int i =0; i<DESC_NUM; i++){
                     observation[i] = descr[j][i][k];
                 }
                 mhhmm->filter(observation);
             }
-            char predicted=mhhmm->results.likeliest[0];
-            
+            predicted=mhhmm->results.likeliest[0];
             std::cout<<"Predicted: "<<predicted<<", Actual: "<<labels[j]<<std::endl;
             if(predicted==labels[j]){
                 accuracy++;
@@ -124,9 +121,9 @@ int runXMM(void* descptr, int sample_size, void* model){
     xmm::HierarchicalHMM* mhhmm = static_cast<xmm::HierarchicalHMM*>(model);
     const float** descr = static_cast<const float**>(descptr);
     
+    mhhmm->reset();
     for(int k=0; k<sample_size;k++){
-        std::vector<float> observation = *new std::vector<float>(9);
-        mhhmm->reset();
+        std::vector<float> observation = *new std::vector<float>(DESC_NUM);
 
         for(int i =0; i<9; i++){
             observation[i] = descr[i][k];
@@ -136,18 +133,18 @@ int runXMM(void* descptr, int sample_size, void* model){
     return mhhmm->results.likeliest[0];
 }
 
-           
-           
-           
 
-//                  SAVE TO JSON
-//
-//        std::ofstream file_id;
-//        file_id.open("/Users/best/Desktop/model.json");
-//        Json::FastWriter writer;
-//        std::cout << writer.write(mhhmm.toJson());
-//        file_id << writer.write(mhhmm.toJson());
-//        file_id.close();
+int save_model_JSON(char* pathptr, void* model){
+    xmm::HierarchicalHMM* mhhmm = static_cast<xmm::HierarchicalHMM*>(model);
+    const char* path = static_cast<const char*>(pathptr);
+    
+    std::ofstream file_id;
+    file_id.open(path);
+    Json::FastWriter writer;
+    file_id << writer.write(mhhmm->toJson());
+    file_id.close();
+    return 'Y';
+}
            
            
 
